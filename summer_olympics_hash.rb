88f7d188d0ae59ba65c6bc363da53036ee0d6184
rraghoth <rraghoth@gmail.com>
2018-07-17 15:52:42 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,17 @@
+summer_olympics = {
+  :Sydney => 2000,
+  :Athens => 2004,
+  :Beijing => 2008,
+  :London => 2012
+}
 
-def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
+summer_olympics[:Atlanta] = 1999
+place = [summer_olympics.keys]
+year = [summer_olympics.values]
+
+summer_olympics.each do |place, year|
+  puts "The #{place} summer olympics tool place in #{year}."
 end
 
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
-
-def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-end
-
-def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-end
+upcased_cities = [place]
+puts upcased_cities
